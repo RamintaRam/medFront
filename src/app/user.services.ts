585ac0bd675321d82eefec348c84497bcca5498e
RamintaRam <raminta5@yahoo.com>
2017-09-07 14:03:15 +0300

    commit

--- src/app/user.services.ts
+++ src/app/user.services.ts
@@ -1,6 +1,17 @@
 import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/Rx';
 
 @Injectable()
-export class UserService{
+export class UserService {
+    constructor(private http: Http) {
 
+    }
+
+    getUsers() {
+        return this.http.get('http://localhost:4200/api/users')
+            .map((response: Response) => {
+                return response.json().users;
+            });
+    }
 }
