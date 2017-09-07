4e48876434076109005f852517ed807b15041019
RamintaRam <raminta5@yahoo.com>
2017-09-07 13:53:22 +0300

    Initial commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import {Routes, RouterModule} from '@angular/router';
+import {AllUsersComponent} from './admin/users/all-users/all-users.component';
+import {ModuleWithProviders} from '@angular/core';
+
+const  APP_ROUTES: Routes = [
+    {path: 'admin/users', component: AllUsersComponent}
+];
+
+export const routing:ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
