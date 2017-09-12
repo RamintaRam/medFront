e0b89fba7a68c6f2b3fffb784adbdcf2800bb8d5
RamintaRam <raminta5@yahoo.com>
2017-09-12 09:46:00 +0300

    commit

--- src/app/admin/auth/signin/signin.component.ts
+++ src/app/admin/auth/signin/signin.component.ts
@@ -1,31 +1,26 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../../auth.service';
 import {NgForm} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html',
     styleUrls: ['./signin.component.css']
-
 })
 export class SigninComponent implements OnInit {
 
-    constructor(private authService: AuthService, private router: Router) {
-    }
+    constructor(private authService: AuthService, private router: Router) { }
 
     ngOnInit() {
         if (this.authService.getToken()) {
-            this.router.navigate(['admin/dashboard']);
+            this.router.navigate(['admin']);
         }
     }
-
     onSignin(form: NgForm) {
         this.authService.signin(form.value.email, form.value.password).subscribe(
-            response => this.router.navigate(['admin/dashboard']),
+            response => this.router.navigate(['admin']),
             error => console.log(error)
         );
-
     }
-
 }
--- src/app/admin/auth/signout/signout.component.ts
+++ src/app/admin/auth/signout/signout.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 
-
 @Component({
   selector: 'app-signout',
-  template: '',
-
+  template: '#'
 })
 export class SignoutComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
     localStorage.removeItem('token');
